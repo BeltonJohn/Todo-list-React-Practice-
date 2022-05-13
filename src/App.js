942eae63032e6911f2d23b0183e02679340397c2
Matt Creese <mattycreese@gmail.com>
2022-05-13 14:46:07 +0100

    second commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+// import TodoForm from './components/TodoForm';
+import TodoList from './components/TodoList';
+
+function App() {
+  return (
+    <div className='todo-app'>
+      <TodoList />
+    </div>
+  );
+}
+export default App;
